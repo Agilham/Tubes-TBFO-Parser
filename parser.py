7c4f6d8af9f0441e00d9cfe7c18510064f7816ac
Agilham <13521118@std.stei.itb.ac.id>
2022-11-21 14:11:48 +0700

    update parser

--- parser.py
+++ parser.py
@@ -1,7 +1,11 @@
 dict = {}
+var = []
+term = []
 
 with open('tes.txt') as f:
     list = f.read().splitlines()
+
+print(list)
 
 maxRow = len(list)
 for i in range(maxRow):
@@ -38,17 +42,23 @@ for i in range(maxRow):
             if (foundVal):
                 if (val != ""):
                     dict[key][prod].append(val)
+                    if (len(val) > 1):
+                        var.append(val)
+                    else:
+                        if (val.isupper()):
+                            var.append(val)
+                        else:
+                            term.append(val)
                 elif (val == ""):
                     if (char == '|'):
                         dict[key].append([])
                         prod += 1
-                    elif (word == ' '):
-                        word = ""
 
-print("Produksi")
-i = 1
-for key, value in dict.items():
-    print(f"Produksi ke-{i}:")
-    print(f"{key} : {value}")
-    i += 1
+# i = 1
+# for key, value in dict.items():
+#     print(f"Produksi ke-{i}:")
+#     print(f"{key} : {value}")
+#     i += 1
 
+print(var)
+print(term)
