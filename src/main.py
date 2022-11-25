59a40e44acf04b4f24d9f2da02deaf2eaa32dd96
Muhammad Habibi Husni <habibihusni18@gmail.com>
2022-11-25 10:25:41 +0700

    menambahkan pembaca file js

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from CFG import *
 from CFGtoCNF import *
 from Cyk_pharser import *
+import re
 
-newCFG = CFG()
-newCFG.readCFG('test/tesCYK.txt')
-newCFG.print()
-CFGtoCNF(newCFG)
-newCFG.print()
-cykPharser("ababa",newCFG)
+filename = input("nama file input: ")
+with open("test/"+filename,'r') as file:
+    fileString = file.read()
+fileString = re.sub("\t","",fileString)
+fileString = re.sub(" {2,}"," ",fileString)
+fileString = re.sub("\n{2,}","\n",fileString)
+print(repr(fileString))
--- src/test.py
+++ src/test.py
@@ -1,21 +1,10 @@
-def getCombinationOfNullable(RHS, nullable):
-    newProduct = set()
-    if (len(RHS) != 0):
-        combTail = getCombinationOfNullable(RHS[1:],nullable)
-        if RHS[0] in nullable:
-            newProduct = newProduct.union(combTail)
-        for comb in combTail:
-            newProduct.add((RHS[0],)+comb)
-    else:
-        newProduct.add(tuple())
-    return newProduct
-class test:
-    x = 5
+from CFG import *
+from CFGtoCNF import *
+from Cyk_pharser import *
 
-def changeVal(t, new):
-    t.x = new    
-
-wow = test()
-print(wow.x)
-changeVal(wow,10)
-print(wow.x)
+newCFG = CFG()
+newCFG.readCFG('test/tesCYK.txt')
+newCFG.print()
+CFGtoCNF(newCFG)
+newCFG.print()
+cykPharser("ababa",newCFG)
