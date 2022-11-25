b5f7fa00a6c79c143c97f805e0470703384c4578
Agilham <13521118@std.stei.itb.ac.id>
2022-11-26 01:54:34 +0700

    update doc, FA

--- src/FA.py
+++ src/FA.py
@@ -28,16 +28,19 @@ def FAvariabel(input):
     
 def FAoperasi(input):
     # belum seleai
-    transisi = {'q0' : {(' ','q0'),('-','q2'),('!','q2')(lowercase+uppercase+otherSym+number,'q1')},
-    'q1' : {(lowercase+uppercase+otherSym+number,'q1'),('$','q1'),(lowercase,'q1'),(uppercase,'q1')}}
-    finalState = set(['q0',"???"])
+    transisi = {'q0' : {(' ','q0'),('-','q2'),('!','q2'),(lowercase+uppercase+otherSym+number,'q1')},
+    'q1' : {(lowercase+uppercase+otherSym+number,'q1'),('$','q1'),(lowercase,'q1'),(uppercase,'q1'),(operator,'q2')},
+    'q2' : {(lowercase+uppercase+otherSym+number,'q3')},
+    'q3' : {(lowercase+uppercase+otherSym+number,'q3')}}
+    finalState = set(['q3'])
     i = 0
     state = 'q0'
     while (i < len(input)):
         found = False
+        print(state)
         for trans in transisi[state]:
-            for i in range(len(trans[0])):
-                if (input[i] == trans[0][i]):
+            for j in range(len(trans[0])):
+                if (input[i] == trans[0][j]):
                     state = trans[1]
                     found = True
         if (found):
