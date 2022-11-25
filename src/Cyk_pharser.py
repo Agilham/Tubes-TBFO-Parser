e899e9f64a1c641732ee11d8268fbdb6316dfc40
Muhammad Habibi Husni <habibihusni18@gmail.com>
2022-11-25 19:07:37 +0700

    CYK fix

--- src/Cyk_pharser.py
+++ src/Cyk_pharser.py
@@ -2,28 +2,29 @@ from CFG import *
 
 def cykPharser(input, cnfGrammar:CFG) : 
     Ni = len(input); 
-    cyktab = [[set() for i in range(Ni)] for j in range(Ni)]
+    cyktab = [[set() for j in range(Ni)] for i in range(Ni+1)]
 
     for i in range(Ni): 
         for LHS in cnfGrammar.produksi : 
             for product in cnfGrammar.produksi[LHS] : 
                 if len(product) == 1 and product[0] == input[i] : 
-                    cyktab[i][i].add(product[0])
+                    cyktab[1][i].add(LHS)
 
-    for s in range(2,Ni+1) : 
-        for i in range(0,Ni-s+1) : 
-            j = i+s-1
-            for z in range(i,j) : 
+    for l in range(2,Ni+1) : 
+        for i in range(0,Ni-l+1):
+            for l1 in range(1,l) : 
+                l2 = l-l1
                 for LHS in cnfGrammar.produksi : 
                     for product in cnfGrammar.produksi[LHS]: 
                         if len(product)==2 : 
-                            if (product[0] in cyktab[i][z]) and (product[1] in cyktab[z+1][j]) : 
-                                cyktab[i][j].add(LHS)
-    for i in range(Ni):
+                            if (product[0] in cyktab[l1][i]) and (product[1] in cyktab[l2][i+l1]): 
+                                cyktab[l][i].add(LHS)
+    # buat testing
+    for i in range(Ni,0,-1):
         for j in range(Ni):
             print(cyktab[i][j],end='')
         print()
-    if "SS" in cyktab[0][Ni-1] : 
+    if "SS" in cyktab[Ni][0] : 
         print("accepted input!")
     else: 
         print("syntax error! ")
